47d9039bbfecc253aa61e257412fbff531fe7a78
vikramtank <vikram.tank@gmail.com>
2009-02-23 18:46:57 -0500

    red square 1 fing

---
+++ MyCustomView.h
@@ -0,0 +1,25 @@
+//
+//  MyCustonView.h
+//  RedSquare
+//
+//  Created by vikramtank on 2/23/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MyCustomView : UIView <UIAccelerometerDelegate> {
+	CGFloat                    squareSize;
+	CGFloat                    rotation;
+	CGFloat					   centerx;
+	CGFloat					   centery;
+	CGColorRef                 aColor;
+	BOOL                       twoFingers;
+	
+	IBOutlet UILabel           *xField;
+	IBOutlet UILabel           *yField;
+	IBOutlet UILabel           *zField;
+}
+
+@end
